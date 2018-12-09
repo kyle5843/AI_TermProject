6f728e8b60201314705d1c4ff743a4e50843c1d8
GuanNJ <44775757+GuanNJ@users.noreply.github.com>
2018-12-09 16:29:55 +0800

    GUI+邏輯檔

---
+++ AI_TermProject/CallAI_GUI_1071209.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import sys 	
+from PyQt5.QtWidgets import QApplication , QMainWindow
+from AI_GUI_1071209 import *
+
+
+class MyMainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):    
+        super(MyMainWindow, self).__init__(parent)
+        self.setupUi(self)
+        
+            
+if __name__=="__main__":  
+    app = QApplication(sys.argv)  
+    myWin = MyMainWindow()  
+    myWin.show()  
+    sys.exit(app.exec_())  
